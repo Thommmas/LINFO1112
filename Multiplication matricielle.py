f90dd711f129eb98eb32a296f9c5e253b267e3db
Thomas <thomas.schiltz@student.uclouvain.be>
2023-04-22 02:53:22 +0200

    Start

--- Multiplication matricielle.py
+++ Multiplication matricielle.py
@@ -1,2 +1,23 @@
 def multiply(A,B):
-    pass
+    try:
+        multiplied = []
+        for n in len(A):
+            for m in len(B[0]):
+                for u in len(A[n]):
+                    multiplied.append(A[n][u]*B[u][n])
+        return multiplied
+
+    except:
+        if len(A[0])!=len(B):  # Compatibility check
+            raise Exception('Dimension mismatch')
+        return Exception
+        
+
+A = [[1,2,3,4,5]]
+B = [[10],
+     [20],
+     [30],
+     [40],     
+     [50]]
+
+print(multiply(A,B))
